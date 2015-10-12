b06c627947f760826672c34b5856737c51c39863
Austin Garrard <wootasaurus@gmail.edu>
2015-10-11 23:45:24 -0500

    made resource inherit blog

--- src/resource.py
+++ src/resource.py
@@ -1,13 +1,10 @@
 from random import randint, uniform
+from blob import Blob
 
-class Resource:
-  def __init__(self, xy, radius, value):
-    self.xy = xy
-    self.radius = radius
+class Resource(Blob):
+  def __init__(self, value, xy=[40.0,20.0], radius = .2, color = (255,255,0)):
+    super(Resource, self).__init__(xy, radius, color)
     self.value = value
-
-
-
 
 
 class ResourceFactory:
@@ -21,47 +18,28 @@ class ResourceFactory:
   def createInitialResources(self):
     self.createResource(self.maxResources/2)
 
-  #generate a number of resources
-  def generateResources(self, numResources):
-    numNeedResources = self.maxResources - numResources;
-    for i in range(numNeedResources/self.maxResources):
-      self.map.addResource(self.createResource())
-
   #randomly create a default resource
   def createResource(self, N=1):
-    n = min(N, self.maxResources - len(self.map.resources))
+    n = min(N, self.maxResources - self.map.numResources)
     for i in range(n):
       while True:
         x = uniform(0, self.map.width)
         y = uniform(0, self.map.width)
         if self.map.validPosition((x,y)):
           value = uniform(self.minValue, self.maxValue)
-          self.map.addResource(Resource((x,y), value, value))
+          self.map.addResource( Resource(
+                                value, [x,y], value, (0,255,0)
+                              ) )
           break
 
   #create a default resource in a given area
-  def createResourceInArea(self, pos, dim, N=1):
-    n = min(N, self.maxResources - len(self.map.resources))
-    for i in range(n):
-      while True:
-        x = uniform(int(pos[0]), int(pos[0] + dim[0]))
-        y = uniform(int(pos[1]), int(pos[1] + dim[1]))
-        if self.map.validPosition((x,y)):
-          value = uniform(self.minValue, self.maxValue)
-          self.map.addResource(Resource((x,y), value, value))
-          break
-
-  #create a randomly placed, specifically valued resource
-  def createResourceWithValue(self, value):
-    while True:
-      x = randint(0, self.map.width)
-      y = randint(0, self.map.width)
-      if self.map.validPosition((x,y)):
-        return Resource((x,y), value, value)
-
-  def createResourceInAreaWithValue(self, pos, dim, value):
-    while True:
-      x = randint(pos[0], pos[0] + dim[0])
-      y = randint(pos[1], pos[1] + dim[1])
-      if self.map.validPosition((x,y)):
-        return Resource((x,y), value, value)
+  # def createResourceInArea(self, pos, dim, N=1):
+  #   n = min(N, self.maxResources - len(self.map.resources))
+  #   for i in range(n):
+  #     while True:
+  #       x = uniform(int(pos[0]), int(pos[0] + dim[0]))
+  #       y = uniform(int(pos[1]), int(pos[1] + dim[1]))
+  #       if self.map.validPosition((x,y)):
+  #         value = uniform(self.minValue, self.maxValue)
+  #         self.map.addResource(Resource((x,y), value, value))
+  #         break
